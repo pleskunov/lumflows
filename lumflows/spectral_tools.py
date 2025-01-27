Bugfix: length units, propagation angle

lumflows/spectral_tools.py
@@ -7,8 +7,8 @@ def compute_with_backside(wavelength, R_f, T_f, R_r, T_r, N_substrate, d_substra
     R_corr = []
     T_corr = []
     n_air = 1.003                              # air
-    d = d_substrate / 1000.0                   # substrate thickness to m
-    propagation_angle = 90                     # TODO: check 
+    d = d_substrate * 1e6                      # substrate thickness to nm
+    propagation_angle = 0                      # normal incidence
     beta = []                                  # absoprtion term
 
     for i in range(len(wavelength)):
@@ -20,7 +20,7 @@ def compute_with_backside(wavelength, R_f, T_f, R_r, T_r, N_substrate, d_substra
 
         # Absorption term
         _alpha = N_substrate[i] * math.sin(math.radians(propagation_angle))        
-        _beta = np.imag(((2 * np.pi) / wavelength[i]) * np.sqrt(N_substrate[i]**2 - _alpha**2 * d))
+        _beta = np.imag(((2 * np.pi) / wavelength[i]) * np.sqrt(N_substrate[i]**2 - _alpha**2) * d)
         beta.append(_beta)
 
     # Backside correction