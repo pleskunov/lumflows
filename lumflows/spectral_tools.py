9d046df85e346864371291432e21b8bac88b2194
Paul Pleskunov <pp@positroncloud.com>
2025-01-31 18:36:22 -0500

    Fully re-written correction algorithm, db of constants and substrate spectra

--- lumflows/spectral_tools.py
+++ lumflows/spectral_tools.py
@@ -1,59 +1,16 @@
-import math
-import cmath
-import numpy as np
+from .utils import *
 
-def compute_with_backside(wvls, R_f, T_f, R_r, T_r, N_sub):
+def compute_with_backside(wvls, R_front, T_front, R_front_reverse, T_front_reverse, substrate_name = "B270"):
 
-    if len(wvls) != len(N_sub):
-        raise ValueError("The lengths of wavelengths and refractive index of the substrate must be equal!")
+    # Prepare the substrate optical constants and spectral data (R_backside, T_backside)
+    N_substrate = interpolate_substrate_constants(read_mat_file(substrate_name), wvls)
+    R_back, T_back = interpolate_substrate_spectral_data(read_spectrum_file(substrate_name), wvls)
 
-    # Constants
-    two_pi: float    = 2.0 * math.pi
-    n_air: float     = 1.003
-    thickness: float = 1000000.0
-    theta: float     = 0.0
-    
-    R_forward = np.array(R_f, dtype=float).flatten()
-    T_forward = np.array(T_f, dtype=float).flatten()
-    R_reverse = np.array(R_r, dtype=float).flatten()
-    T_reverse = np.array(T_r, dtype=float).flatten()
+    # Compute absoprtion term
+    beta = compute_absoprtion_term(wvls, N_substrate)
 
-    # Compute R and T for the air-glass interface at the backside of the substrate, using Fresnel eqs.
-    R_back = np.zeros_like(wvls, dtype=float)
-    T_back = np.zeros_like(R_back, dtype=float)
-    for i in range(len(wvls)):
-        n = np.real(N_sub[i])
-        R_back[i] = np.abs((n - n_air) / (n + n_air))**2
-        T_back[i] = 1.0 - R_back[i]
+    # Compute corrected R and T spectra
+    R = compute_R_with_backside(wvls, R_front, T_front, R_front_reverse, T_front_reverse, R_back, beta)
+    T = compute_T_with_backside(wvls, T_front, R_front_reverse, T_back, R_back, beta)
 
-    # Compute the substrate absoprtion term
-    beta_i = np.zeros_like(wvls, dtype=float)
-    sin_theta: float = math.sin(math.radians(theta))
-
-    for i in range(len(wvls)):
-        # alpha squared
-        n_sin_theta: complex = N_sub[i] * sin_theta
-        sin2: complex = n_sin_theta * n_sin_theta
-
-        N_square: complex = N_sub[i] * N_sub[i]
-        N_s_s: complex = cmath.sqrt(N_square - sin2)
-
-        # Correct branch selection
-        if N_s_s.real == 0.0:
-            N_s_s = -N_s_s
-
-        beta_i[i] = np.imag(two_pi * thickness * N_s_s / wvls[i])
-        
-        # Debug
-        print(beta_i[i])
-
-    # Compute R and T spectra with backside correction
-    R_corrected = np.zeros_like(R_forward, dtype=float)
-    T_corrected = np.zeros_like(R_corrected, dtype=float)
-
-    for i in range(len(wvls)):
-        R_corrected[i] = R_forward[i] + (T_forward[i] * T_reverse[i] * R_back[i] * math.exp(4.0 * beta_i[i])) / (1.0 - R_reverse[i] * R_back[i] * math.exp(4.0 * beta_i[i]))
-        
-        T_corrected[i] = (T_forward[i] * T_back[i] * math.exp(2.0 * beta_i[i])) / (1.0 - R_reverse[i] * R_back[i] * math.exp(4.0 * beta_i[i]))
-
-    return R_corrected, T_corrected
+    return R, T
---
+++ lumflows/utils.py
@@ -0,0 +1,99 @@
+import os
+import numpy as np
+from math import pi, sin, radians, exp
+from cmath import sqrt
+
+DISPERSION_SUFFIX = "_nk"
+SPECTRAL_DATA_SUFFIX = "_rt"
+EXTENSION = ".txt"
+
+def zeros_like(array):
+    return np.zeros_like(array, dtype=float)
+
+def _get_root_dir():
+    return os.path.dirname(os.path.abspath(__file__))
+
+def _get_mat_file(filename):
+    root = _get_root_dir()
+    file = os.path.join(root, "db", filename)
+    if not os.path.exists(file):
+        raise FileNotFoundError(f"File '{filename}' not found in the database!")
+    
+    return file
+
+def read_mat_file(filename):
+    file = _get_mat_file(filename + DISPERSION_SUFFIX + EXTENSION)
+    
+    return np.loadtxt(file, delimiter="\t", skiprows=1).transpose() # TODO: remove any formatting, must be taken care by user
+
+def read_spectrum_file(filename):
+    file = _get_mat_file(filename + SPECTRAL_DATA_SUFFIX + EXTENSION)
+    
+    return np.loadtxt(file).transpose()
+
+def _interpolate(wvls, data, new_wvls):
+    return np.interp(new_wvls, wvls, data)
+
+def interpolate_substrate_constants(substrate_constants, new_wvls):
+    init_wvls, n, k = substrate_constants[0], substrate_constants[1], substrate_constants[2]
+
+    n = _interpolate(init_wvls, n, new_wvls)
+    k = _interpolate(init_wvls, k, new_wvls)
+    N = n - k * 1j
+
+    return N
+
+def interpolate_substrate_spectral_data(substrate_spectra, new_wvls):
+    init_wvls, r, t = substrate_spectra[0], substrate_spectra[1], substrate_spectra[2]
+
+    r = _interpolate(init_wvls, r, new_wvls)
+    t = _interpolate(init_wvls, t, new_wvls)
+
+    return r, t
+
+def _compute_beta(wvls, N, theta, thickness):
+    two_pi = 2 * pi
+    # Compute the substrate absoprtion term
+    beta_i = zeros_like(wvls)
+    sin_theta: float = sin(radians(theta))
+
+    for i in range(len(wvls)):
+        # alpha squared
+        n_sin_theta: complex = N[i] * sin_theta
+        sin2: complex = n_sin_theta * n_sin_theta
+        
+        N_square: complex = N[i] * N[i]
+        N_s_s: complex = sqrt(N_square - sin2)
+    
+        # Correct branch selection
+        if N_s_s.real == 0.0:
+            N_s_s = -N_s_s
+    
+        beta_i[i] = np.imag(two_pi * thickness * N_s_s / wvls[i])
+
+    return beta_i
+
+def compute_absoprtion_term(wvls, N, theta = 0.0, thickness = 2000000.0):
+    return _compute_beta(wvls=wvls, N=N, theta=theta, thickness=thickness)
+
+def _T_with_backside(T_front, R_front_reverse, T_back, R_back, beta):
+    return (T_front * T_back * exp(2.0*beta)) / (1.0 - R_front_reverse * R_back * exp(4.0*beta))
+
+def _R_with_backside(R_front, T_front, R_front_reverse, T_front_reverse, R_back, beta):
+    return R_front + ((T_front * T_front_reverse * R_back * exp(4.0*beta)) / (1.0 - R_front_reverse * R_back * exp(4.0*beta)))
+
+def compute_T_with_backside(wvls, T_front, R_front_reverse, T_back, R_back, beta):
+    T = zeros_like(wvls)
+
+    for i in range(len(wvls)):
+        T[i] = _T_with_backside(T_front=T_front[i], R_front_reverse=R_front_reverse[i], T_back=T_back[i], R_back=R_back[i], beta=beta[i])
+
+    return T
+
+def compute_R_with_backside(wvls, R_front, T_front, R_front_reverse, T_front_reverse, R_back, beta):
+    R = zeros_like(wvls)
+
+    for i in range(len(wvls)):
+        R[i] = _R_with_backside(R_front=R_front[i], T_front=T_front[i], R_front_reverse=R_front_reverse[i], T_front_reverse=T_front_reverse[i], R_back=R_back[i], beta=beta[i])
+
+    return R
