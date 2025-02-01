3db77bcdf07c49d5622fc0a4b1278b42e76f09dd
Paul Pleskunov <pp@positroncloud.com>
2025-02-01 17:19:42 -0500

    R and T for backside iface are computed independently

--- lumflows/spectral_tools.py
+++ lumflows/spectral_tools.py
@@ -1,10 +1,19 @@
 from .utils import *
 
-def compute_with_backside(wvls, R_front, T_front, R_front_reverse, T_front_reverse, substrate_name = "B270"):
+def compute_with_backside(wvls, R_front, T_front, R_front_reverse, T_front_reverse, substrate_name = "B270", N_substrate = None):
 
-    # Prepare the substrate optical constants and spectral data (R_backside, T_backside)
-    N_substrate = interpolate_substrate_constants(read_mat_file(substrate_name), wvls)
-    R_back, T_back = interpolate_substrate_spectral_data(read_spectrum_file(substrate_name), wvls)
+    # Prepare the substrate optical constants
+    if N_substrate is None:
+        N_substrate = interpolate_substrate_constants(read_mat_file(substrate_name), wvls)
+    else:
+        if len(wvls) != len(N_substrate):
+            raise RuntimeError("Lengths of wavelengths and complex index refraction values must be the same.")
+
+        if not np.issubdtype(N_substrate.dtype, np.complexfloating):
+            raise TypeError("Refractive index of the substrate must be in the complex form.")
+    
+    # Compute the substrate spectra (R_backside, T_backside)
+    R_back, T_back = compute_substrate_spectra(wvls, N_substrate)
 
     # Compute absoprtion term
     beta = compute_absoprtion_term(wvls, N_substrate)
--- lumflows/utils.py
+++ lumflows/utils.py
@@ -4,7 +4,7 @@ from math import pi, sin, radians, exp
 from cmath import sqrt
 
 DISPERSION_SUFFIX = "_nk"
-SPECTRAL_DATA_SUFFIX = "_rt"
+#SPECTRAL_DATA_SUFFIX = "_rt" # Deprecated because we calculate R and T at the backside interface ourselves
 EXTENSION = ".txt"
 
 def zeros_like(array):
@@ -26,16 +26,21 @@ def read_mat_file(filename):
     
     return np.loadtxt(file, delimiter="\t", skiprows=1).transpose() # TODO: remove any formatting, must be taken care by user
 
-def read_spectrum_file(filename):
-    file = _get_mat_file(filename + SPECTRAL_DATA_SUFFIX + EXTENSION)
-    
-    return np.loadtxt(file).transpose()
+# Deprecated because we calculate R and T at the backside interface ourselves
+#def read_spectrum_file(filename):
+#    file = _get_mat_file(filename + SPECTRAL_DATA_SUFFIX + EXTENSION)
+#    
+#    return np.loadtxt(file).transpose()
 
 def _interpolate(wvls, data, new_wvls):
     return np.interp(new_wvls, wvls, data)
 
 def interpolate_substrate_constants(substrate_constants, new_wvls):
-    init_wvls, n, k = substrate_constants[0], substrate_constants[1], substrate_constants[2]
+
+    if substrate_constants.shape[0] > substrate_constants.shape[1]:
+        raise RuntimeError("The substrate optical properties array is not properly formatted!")
+    else:
+        init_wvls, n, k = substrate_constants[0], substrate_constants[1], substrate_constants[2]
 
     n = _interpolate(init_wvls, n, new_wvls)
     k = _interpolate(init_wvls, k, new_wvls)
@@ -43,13 +48,37 @@ def interpolate_substrate_constants(substrate_constants, new_wvls):
 
     return N
 
-def interpolate_substrate_spectral_data(substrate_spectra, new_wvls):
-    init_wvls, r, t = substrate_spectra[0], substrate_spectra[1], substrate_spectra[2]
+# Deprecated because we calculate R and T at the backside interface ourselves
+#def interpolate_substrate_spectral_data(substrate_spectra, new_wvls):
+#    init_wvls, r, t = substrate_spectra[0], substrate_spectra[1], substrate_spectra[2]
+#
+#    r = _interpolate(init_wvls, r, new_wvls)
+#    t = _interpolate(init_wvls, t, new_wvls)
+#
+#    return r, t
 
-    r = _interpolate(init_wvls, r, new_wvls)
-    t = _interpolate(init_wvls, t, new_wvls)
+def _compute_R_backside(N_substrate: complex, n_medium: float = 1.0003):
+    # Assume normal incidence
+    n_substrate: float = np.real(N_substrate)
 
-    return r, t
+    numerator = n_medium - n_substrate
+    denominator = n_medium + n_substrate
+    R = np.abs(numerator/denominator)
+
+    return R * R
+
+def _compute_T_backside(R_back):
+    return 1.0 - R_back
+
+def compute_substrate_spectra(wvls, N_substrate):
+    R_back = zeros_like(wvls)
+    T_back = zeros_like(R_back)
+
+    for i in range(len(wvls)):
+        R_back[i] = _compute_R_backside(N_substrate[i])
+        T_back[i] = _compute_T_backside(R_back[i])
+
+    return R_back, T_back
 
 def _compute_beta(wvls, N, theta, thickness):
     two_pi = 2 * pi
