89faf3ae4350b719412ac7909864f3f2cbf7a55c
Paul Pleskunov <pp@positroncloud.com>
2025-03-27 21:26:28 -0400

    Cleanup - local session management support

--- lumflows/api.py
+++ lumflows/api.py
@@ -1,53 +1,76 @@
-from lumapi_connector import connector # type: ignore
+from session import Connector
 from .definitions import *
 from .spectral_tools import freq_to_wavelength
 
-class API():
-    def __init__(self, fdtd = None, units = NANO):
-        if fdtd is None:
-            api_connector = connector.LumAPI()
-            self.api = api_connector.connect()        
-            self.fdtd = self.api.FDTD()
-        else:
-            self.api = None
-            self.fdtd = fdtd
-            self.units = units
-            self.monitors = []
+class FDTD:
+    ######################################################################
+	#                                                                    #
+	# __init__                                                           #
+	#                                                                    #
+	######################################################################
+    def __init__(self, filename = None, hide = False, serverArgs = {}, remoteArgs = {}, units = NANO):
+        """
+        Launches a new FDTD session.
 
+        Parameters:
+        -----------
+        filename: str (optional)
+            A single string containing either as script filename or a project filename. 
+            When the parameter is a project filename, the project will be loaded. 
+            When the parameter is a script filename, it will be evaluated. 
+        
+        hide: bool (optional, default is false)
+            Shows or hides the Lumerical GUI/CAD environment on startup. 
+            When set to True, all pop-up messages that normally appear in the GUI will also be disabled.
+
+        serverArgs: dict (optional)
+            Pass command line arguments to the product during launch as a dictionary.
+
+            Example: serverArgs = {'use-solve':True, 'platform':'offscreen', 'threads': '2’}
+        
+        remoteArgs: dict
+            Pass connection information as a dictionary. 
+            Only used when using the Python API remotely on a Linux machine that is 
+            running the Interop Server.
+
+            Example:
+                remoteArgs = { "hostname": "192.168.215.129", "port": 8989 }
+
+        units: float (optional, default is nm)
+            Units multplier used for setting object dimensions and wavelengths.
+        """
+        connector = Connector
+        self.api = connector.connect()        
+        self.fdtd = self.api.FDTD(filename=filename, hide=hide, serverArgs=serverArgs, remoteArgs=remoteArgs)
+
+        self.units = units
+        self.monitors = []
 
     def _update_units(self, **kwargs):
         return {key: value * self.units if isinstance(value, (int, float)) else value for key, value in kwargs.items()}
-
 
     def load_project(self, file):
         print("Loading the project...")
         self.fdtd.load(file + ".fsp")
 
-
     def run(self):
         self.fdtd.run()
-
 
     def switch_to_layout(self):
         self.fdtd.switchtolayout()
 
-
     def disable(self, name):
         self.fdtd.setnamed(name, "enabled", 0)
 
-
     def enable(self, name):
         self.fdtd.setnamed(name, "enabled", 1)
-
 
     def get_wavelengths(self, monitor_name):
         frequency = self.fdtd.getdata(monitor_name, "f")
         return freq_to_wavelength(frequency)
 
-
     def get_transmitted_power(self, monitor_name):
         return self.fdtd.transmission(monitor_name)
-
 
     def add_fdtd_region_with_span(self, dimension, **kwargs):
         """
@@ -66,7 +89,6 @@ class API():
         """
         self.fdtd.addfdtd(dimension=dimension, **self._update_units(**kwargs))
 
-
     def add_fdtd_region_with_min_max(self, dimension, **kwargs):
         """
         Add an FDTD region defining its geometry using the min-max coordinates.
@@ -84,7 +106,6 @@ class API():
         """
         self.fdtd.addfdtd(dimension=dimension, **self._update_units(**kwargs))
 
-
     def set_mesh_type(self, mesh_type = MESH_AUTO):
         """
         Set mesh type.
@@ -95,7 +116,6 @@ class API():
             Type of the mesh to be used (e.g. "auto non-uniform" or "uniform").
         """
         self.fdtd.setnamed(FDTD_DOMAIN, MESH_TYPE, mesh_type)
-
 
     def set_mesh_refinement(self, mesh_technology = MESH_C1):
         """
@@ -108,7 +128,6 @@ class API():
         """
         self.fdtd.setnamed(FDTD_DOMAIN, MESH_REFINEMENT, mesh_technology)
 
-
     def set_all_bc_symmetry(self, symmetry: bool):
         """
         Set whether all boundary conditions use symmetry.
@@ -119,7 +138,6 @@ class API():
             If True, all boundary conditions will use symmetry.
         """
         self.fdtd.setnamed(FDTD_DOMAIN, ALL_BC_SYMMETRY, symmetry)
-
 
     def set_bc_x(self, bc_min = BC_PML, bc_max = BC_PML):
         """
@@ -135,7 +153,6 @@ class API():
         self.fdtd.setnamed(FDTD_DOMAIN, BC_X_MIN, bc_min)
         self.fdtd.setnamed(FDTD_DOMAIN, BC_X_MAX, bc_max)
 
-
     def set_bc_y(self, bc_min = BC_PML, bc_max = BC_PML):
         """
         Set boundary conditions for the Y-axis.
@@ -149,7 +166,6 @@ class API():
         """
         self.fdtd.setnamed(FDTD_DOMAIN, BC_Y_MIN, bc_min)
         self.fdtd.setnamed(FDTD_DOMAIN, BC_Y_MAX, bc_max)
-
 
     def set_bc_z(self, bc_min = BC_PML, bc_max = BC_PML):
         """
@@ -165,14 +181,11 @@ class API():
         self.fdtd.setnamed(FDTD_DOMAIN, BC_Z_MIN, bc_min)
         self.fdtd.setnamed(FDTD_DOMAIN, BC_Z_MAX, bc_max)
 
-    
     def set_pml_profile(self, pml_profile = PML_STEEP_ANGLE):
         self.fdtd.setnamed(FDTD_DOMAIN, PML_PROFILE, pml_profile)
 
-
     def set_number_of_pml_layers(self, number_of_layers = 32):
         self.fdtd.setnamed(FDTD_DOMAIN, PML_LAYERS, number_of_layers)
-
 
     def add_power_monitor(self, name, monitor_type, **kwargs):
         """
@@ -201,19 +214,15 @@ class API():
         print("Adding monitor to the simulation...")
         self.monitors.append(self.fdtd.addpower(name=name, monitor_type=monitor_type, **self._update_units(**kwargs)))
 
-    
     def set_global_monitor_option(self, key, value):
         self.fdtd.setglobalmonitor(key, value)
-
 
     def set_number_of_points_globally(self, number_of_points = 701):
         self.set_global_monitor_option(FDP_MONITOR_FREQ_POINTS, number_of_points)
 
-    
     def set_monitor_option(self, monitor_name, option, value):
         self.fdtd.setnamed(monitor_name, option, value)
 
-    
     def add_plane_wave_source(self, name, wavelength_start = 0.21 * 1e-6, wavelength_stop = 2.5 * 1e-6, **kwargs):
         """
         Add a Plane Wave Light Source defining its geometry using the center-span/or min-max coordinates.
@@ -243,18 +252,14 @@ class API():
         """
         self.fdtd.addplane(name=name, wavelength_start=wavelength_start, wavelength_stop=wavelength_stop, **self._update_units(**kwargs))
   
-    
     def set_source_option(self, source_name, option, value):
         self.fdtd.setnamed(source_name, option, value)
-
 
     def set_injection_axis(self, source_name, axis = "z"):
         self.set_source_option(source_name, LIGHT_SRC_INJECTION_AX, axis)
 
-
     def set_injection_direction(self, source_name, direction):
         self.set_source_option(source_name, LIGHT_SRC_INJECTION, direction)
-
 
     def set_source_polarization(self, source_name, polarization_angle):
         self.set_source_option(source_name, LIGHT_SRC_POLARIZATION, polarization_angle)
