422fc0f35fbe24f5376967f36f5ac7d426dbe5b5
Paul Pleskunov <pp@positroncloud.com>
2025-03-27 23:02:35 -0400

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lumflows",
-    version="1.0.0",
+    version="1.0.1",
     author="Pavel Pleskunov",
     author_email="pavel.pleskunov@polymtl.ca",
     description="A collection of post-processing tools for Lumerical FDTD data.",
